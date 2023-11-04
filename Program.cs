a605f93da4133b06a6be18f555d11306d4b97a7d
Administrator <ankitsut@gmail.com>
2023-11-04 11:36:44 +0530

    test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!1111");
+app.MapGet("/", () => "Hello World!1111test");
 
 app.Run();
