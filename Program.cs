45742a4efcf3ce78896ad28045f8d6075567e38d
Administrator <giladmin@gmail.com>
2024-04-29 15:56:17 +0530

    last
    last

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World ankit testawae");
+app.MapGet("/", () => "Hello World ankit last update");
 
 app.Run();
