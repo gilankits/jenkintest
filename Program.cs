dd7ab729e79611e3d70cbd9d8ddedcb472aa9cbc
Administrator <giladmin@gmail.com>
2024-04-29 16:06:04 +0530

    awweqweq
    qweqwe

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!1111test  woasdasdrking");
+app.MapGet("/", () => "Hello World!1111test  woaadawsdasdrking");
 
 app.Run();
