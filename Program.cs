28cc72891bc739f2d156d91ac4f075801438bcd9
Administrator <giladmin@gmail.com>
2024-05-02 19:16:06 +0530

    3242wdasd
    asds

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello a for master branch");
+app.MapGet("/", () => "Hellasdasdasdo a for master branch");
 
 app.Run();
