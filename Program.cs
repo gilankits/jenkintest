f36c5e8d3f665a8106d948c2d78eba5fd2e87d3a
Administrator <giladmin@gmail.com>
2024-05-02 19:12:54 +0530

    ada
    asd

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello asdasdasdasawdaawwWoawdasdasdsdawdrld for master branch");
+app.MapGet("/", () => "Hello a for master branch");
 
 app.Run();
