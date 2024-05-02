069eb44d7294e01375e6e391748d86aef73b5525
Administrator <giladmin@gmail.com>
2024-05-02 18:44:55 +0530

    1121212
    1212121

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Woawdasdasdsdawdrld for master branch");
+app.MapGet("/", () => "Hello awdawwWoawdasdasdsdawdrld for master branch");
 
 app.Run();
