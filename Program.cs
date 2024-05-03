d707fef1dd456cb8a0bc7f6623cfaac926e935ea
Administrator <ankitsut@gmail.com>
2024-05-03 14:41:33 +0530

    test

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/vulnerable", (string data) =>
-                        {
-                            Console.WriteLine($"Received data testing: {data}");
-                            return "Received data";
-                        });
+app.MapGet("/", () => "Hello World");
 app.Run();
